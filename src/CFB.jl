6b64bd653f5bf79ec8f33083981fab6310fe64db
Benjamin James <benjames@mit.edu>
2025-08-06 18:53:20 -0400

    Add drives, reorganize

--- src/CFB.jl
+++ src/CFB.jl
@@ -1,5 +1,13 @@
 module CFB
+using HTTP
+using JSON3
+using SQLite
+using Dates
+using TimeZones
+using DataFrames
+
 include("CfbdClient.jl")
+include("fetch.jl")
 greet() = print("Hello World!")
 
 end # module CFB
